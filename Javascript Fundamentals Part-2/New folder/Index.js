16574a26970bbeed972415281cf53be6506de816
Ritesh Narwade <riteshnarwade03@gmail.com>
2023-11-03 21:45:05 +0530

    Day01 JS_Funda_P-2 Lec-33

---
+++ Javascript Fundamentals Part-2/New folder/Index.js
@@ -0,0 +1,25 @@
+/** 32 Lecture Of Strict Mode */
+
+// 'use strict';
+// let hasDriversLicence = false;
+// const passTest = true;
+// if(passTest) hasDriverLicence=true;
+// if(hasDriversLicence) console.log('I can drive');
+
+/** 33 Functions */
+
+// function logger() {
+//   console.log("Hello my name is Ritesh");
+// }
+
+// logger();
+// logger();
+
+// function fruitProcessor(numOfApples, numOfOranges) {
+//   console.log(numOfApples, numOfOranges);
+//   const juice = `The apples are ${numOfApples} and oranges are ${numOfOranges}`;
+//   return juice;
+// }
+
+// const numb = fruitProcessor(5,0);
+// console.log(numb); 

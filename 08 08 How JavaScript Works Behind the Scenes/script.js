d469a86f3e03d8717a9b4089d1dd185d349754fe
Ritesh Narwade <riteshnarwade03@gmail.com>
2023-11-11 19:32:56 +0530

    Day09 JS_08 How JavaScript Works Behind the Scenes  Lec-91

---
+++ 08 08 How JavaScript Works Behind the Scenes/script.js
@@ -0,0 +1,17 @@
+// const y = 1;
+
+// function first() {
+//   const a = 1;
+//   const b = second(1, 2);
+//   const x = a + b;
+//   return x;
+// }
+
+// function second(a, b) {
+//   const c = a + b;
+//   return c;
+// }
+
+// const x = 1;
+
+// console.log(x, first(), second());

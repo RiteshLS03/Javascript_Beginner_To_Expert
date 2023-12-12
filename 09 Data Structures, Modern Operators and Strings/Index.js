21727800d8a839b2d777c72b98c21087783fd82c
Ritesh Narwade <riteshnarwade03@gmail.com>
2023-12-12 19:07:05 +0530

    Day04 JS_09 Data Structures, Modern Operators and Strings  Lec-111

--- 09 Data Structures, Modern Operators and Strings/Index.js
+++ 09 Data Structures, Modern Operators and Strings/Index.js
@@ -232,4 +232,88 @@ const reastaurant = {
 
 // reastaurant.orderPizza && reastaurant.orderPizza("Mushroom", "Spinach");
 
-// CODING CHALLENGE
+// 109 CODING CHALLENGE
+
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// //1
+// let [players1, players2] = game.players;
+// console.log(players1, players2);
+// //2
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+// //3
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+// //4
+// const players1Final = [...players1, "Thiago", "Coutiinho", "Perisic"];
+// console.log(players1);
+// //5
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
+// //6
+// const printGoals = (...players) => {
+//   console.log(`${players.length} Goals were scored`);
+// };
+
+// printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+// printGoals("Davies", "Muller", "Lewandowski");
+// printGoals("Davies", "Muller");
+
+// //7
+
+// team1 < team2 && console.log("Team 1 is more likely to win");
+// team1 > team2 && console.log("Team 2 is more likely to win");
+
+// 110 Looping Arrays_ The for-of Loop
+
+// const menu = [...reastaurant.starterMenu, ...reastaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+// for (const item of menu.entries()) {
+//   console.log(item);
+// }
+
+// console.log(menu.entries());

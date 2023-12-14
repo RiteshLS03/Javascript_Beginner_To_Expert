d9d8043bd1f28a9b223a747c67b5df22f6db5e26
Ritesh Narwade <riteshnarwade03@gmail.com>
2023-12-14 20:34:21 +0530

    Day05 JS_09 Data Structures, Modern Operators and Strings  Lec-116

--- 09 Data Structures, Modern Operators and Strings/Index.js
+++ 09 Data Structures, Modern Operators and Strings/Index.js
@@ -335,48 +335,77 @@ const reastaurant = {
 
 // 114 CODING CHALLENGE
 
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-const scored = Object.entries(game.scored);
-console.log(scored);
+// const scored = Object.entries(game.scored);
+// console.log(scored);
 
-const 
+// const scored2 = game.scored.entries();
+// console.log(scored2);
+
+// console.log(game.scored.entries());
+// //1
+// for (const [i, player] of game.scored.entries()) {
+//   console.log(`GOAL ${i + 1}: ${player}`);
+// }
+// //2
+// let avgOdds = 0;
+// for (const odd of Object.values(game.odds)) {
+//   avgOdds += odd;
+// }
+// console.log(avgOdds / Object.values(game.odds).length);
+// //3
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === "x" ? "draw" : game[team];
+//   console.log(`odd of ${teamStr} : ${odd}`);
+// }
+
+// 115 Sets
+
+const ordersSet = new Set([
+  "Pizza",
+  "Pasta",
+  "Risotto",
+  "Pizza",
+  "Risotto",
+  "Pizza",
+]);

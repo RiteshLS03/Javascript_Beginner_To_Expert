b12cc10b5e3379891a4cbdf50c70499948fdf793
Ritesh Narwade <riteshnarwade03@gmail.com>
2023-12-22 20:27:35 +0530

    Day10 JS_11 Working_With_Arrays Lec-146

--- 11 Working With Arrays/script.js
+++ 11 Working With Arrays/script.js
@@ -1,76 +1,174 @@
 'use strict';
 
+// // 140 Simple Array Methods
+
+// // 1) Slice() Method
+// // Slice cut down an slice of an array. It return new array without changing original array
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// console.log(arr.slice(2));
+
+// console.log(arr.slice(2, 4));
+
+// console.log(arr.slice(-2));
+
+// // 2) Splice() Method
+
+// // 141 Looping Array forEach
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// //forOf Loop
+// // for (const movement of movements) {
+// for (const [i, movement] of movements.entries()) {
+//   // destructuring with current array
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1} : You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1} : You withdrew ${movement}`);
+//   }
+// }
+
+// //forEach Loop
+// console.log(`---- forEach ----`);
+// movements.forEach(function (movement, i, array) {
+//   // callfunction's parameter's set default
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1} : You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1} : You withdrew ${movement}`);
+//   }
+// });
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
+// const account1 = {
+//   owner: 'Jonas Schmedtmann',
+//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+//   interestRate: 1.2, // %
+//   pin: 1111,
+// };
 
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
+// const account2 = {
+//   owner: 'Jessica Davis',
+//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+//   interestRate: 1.5,
+//   pin: 2222,
+// };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+// const account3 = {
+//   owner: 'Steven Thomas Williams',
+//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//   interestRate: 0.7,
+//   pin: 3333,
+// };
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
+// const account4 = {
+//   owner: 'Sarah Smith',
+//   movements: [430, 1000, 700, 50, 90],
+//   interestRate: 1,
+//   pin: 4444,
+// };
 
-const accounts = [account1, account2, account3, account4];
+// const accounts = [account1, account2, account3, account4];
 
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+// // Elements
+// const labelWelcome = document.querySelector('.welcome');
+// const labelDate = document.querySelector('.date');
+// const labelBalance = document.querySelector('.balance__value');
+// const labelSumIn = document.querySelector('.summary__value--in');
+// const labelSumOut = document.querySelector('.summary__value--out');
+// const labelSumInterest = document.querySelector('.summary__value--interest');
+// const labelTimer = document.querySelector('.timer');
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+// const containerApp = document.querySelector('.app');
+// const containerMovements = document.querySelector('.movements');
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+// const btnLogin = document.querySelector('.login__btn');
+// const btnTransfer = document.querySelector('.form__btn--transfer');
+// const btnLoan = document.querySelector('.form__btn--loan');
+// const btnClose = document.querySelector('.form__btn--close');
+// const btnSort = document.querySelector('.btn--sort');
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+// const inputLoginUsername = document.querySelector('.login__input--user');
+// const inputLoginPin = document.querySelector('.login__input--pin');
+// const inputTransferTo = document.querySelector('.form__input--to');
+// const inputTransferAmount = document.querySelector('.form__input--amount');
+// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+// const inputCloseUsername = document.querySelector('.form__input--user');
+// const inputClosePin = document.querySelector('.form__input--pin');
+
+// const displayMovements = function (movements) {
+//   containerMovements.innerHTML = '';
+
+//   movements.forEach(function (movement, index, movements) {
+//     const type = movement > 0 ? 'deposit' : 'withdrawal';
+
+//     const html = `<div class="movements__row">
+//     <div class="movements__type movements__type--${type}">${
+//       index + 1
+//     } ${type}</div>
+//     <div class="movements__value">${movement}</div>
+//   </div>`;
+//     containerMovements.insertAdjacentHTML('afterbegin', html);
+//   });
+// };
+
+// displayMovements(account1.movements);
+
+// // LECTURES
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// 145 CODING CHALLENGE
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// Coding Challenge #1
 
-/////////////////////////////////////////////////
+/**
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+
+*/
+
+function checkDogs(dogJulia, dogKate) {
+  const dataOfJulia = dogJulia;
+  const shallowOfJulia = dataOfJulia.slice(1, -2);
+  // console.log(dataOfJulia, shallowOfJulia);
+  const bothOfData = shallowOfJulia.concat(dogKate);
+  console.log(bothOfData);
+
+  bothOfData.forEach(function (value, index) {
+    value >= 3
+      ? console.log(
+          `Dog number ${index + 1} is an adult, and is ${value} years old`
+        )
+      : console.log(`Dog number ${index + 1} is still a puppy 🐶`);
+  });
+}
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);

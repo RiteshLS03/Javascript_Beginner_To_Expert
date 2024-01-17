1eb9e1ca8cbd15e6489e5b940987178465db76d3
Ritesh Narwade <riteshnarwade03@gmail.com>
2024-01-17 23:37:33 +0530

    Day30 JS_16_Asynchronous_Javascript_Promises_Async_Await_and_AJAX Lec-260

--- 16 Asynchronous Javascript Promises Async Await and AJAX/script.js
+++ 16 Asynchronous Javascript Promises Async Await and AJAX/script.js
@@ -289,3 +289,70 @@ function getCountry(country) {
 
 // NULL 8
 // NULL 9
+
+// 257
+
+// const whereAmI = async function (country) {
+//   try {
+//     const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+//     const data = await res.json();
+//     console.log(data);
+//     renderCountry(data[0]);
+//   } catch (err) {
+//     console.error(`${err}`);
+//     renderError(`Something went wrong  ${err.message}`);
+//   }
+// };
+
+// whereAmI('usa');
+// whereAmI('usa');
+// whereAmI('usa');
+
+// 258
+
+// 259
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+//     const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+//     const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+//     console.log([data1.capital, data2.capital, data3.capital]);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// get3Countries('usa', 'portugal', 'tanzania');
+
+// 260
+
+// (async function () {
+//   const res = await Promise.race([
+//     getJSON(`https://restcountries.com/v3.1/name/italy`),
+//     getJSON(`https://restcountries.com/v3.1/name/usa`),
+//     getJSON(`https://restcountries.com/v3.1/name/india`),
+//   ]);
+//   console.log(res);
+// })();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long!'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([getJSON(`https://restcountries.com/v3.1/name/italy`), timeout(1)])
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
